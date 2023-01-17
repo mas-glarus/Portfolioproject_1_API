e50c511c1aa17e8bca41ddf4e707486ec331ee14
marc aschwanden <marc.aschwanden88@gmail.com>
2023-01-17 22:14:40 +0100

    first rework after call with abed

--- script.js
+++ script.js
@@ -1,78 +1,30 @@
 const apiKey = "BLMFFWPCzvU5LD1g0rwVrFsznX47BPiRRMhpPtTNyOSktCRiTB";
 const apiSecret = "16ouruHP8NBNMiL4R4ituHEW4GdlgwebiGTFmBah";
-const btn = document.getElementById("btn-submit");
-const output = document.getElementById("output");
-const body = document.querySelector("body");
-const animalList = document.getElementById("animalList");
+const dog = document.querySelector("#animalType__dog");
+const cat = document.querySelector("#animalType__cat");
+const bird = document.querySelector("#animalType__bird");
+let animalType = "";
 
-function onClickHandler() {
-	const selectedItem = document.querySelector("#animalSelection");
-	const animalType = selectedItem.value;
-	output.innerHTML = animalType;
-	if (animalType === "empty") {
-		output.classList.add("redAlert");
-		output.innerHTML = "You have to choose a type";
-		body.classList.remove("lightGreen");
-	} else {
-		console.log(animalType);
-		body.classList.add("lightGreen");
-		output.classList.remove("redAlert");
-		getAnimals(animalType);
-	}
+function onClickHandlerDog() {
+	animalType = "Dog";
+	location.href = "./index_animals.html";
+	sessionStorage.setItem("animal", animalType);
 }
 
-btn.addEventListener("click", onClickHandler);
-// const org = "RI77";
-// const status = "adoptable";
+dog.addEventListener("click", onClickHandlerDog);
 
-// Call the API
-// This is a POST request, because we need the API to generate a new token for us
-
-function getAnimals(animalType) {
-	fetch("https://api.petfinder.com/v2/oauth2/token", {
-		method: "POST",
-		body:
-			"grant_type=client_credentials&client_id=" +
-			apiKey +
-			"&client_secret=" +
-			apiSecret,
-		headers: {
-			"Content-Type": "application/x-www-form-urlencoded",
-		},
-	})
-		.then(function (resp) {
-			// Return the response as JSON
-			return resp.json();
-		})
-		.then(function (data) {
-			// Log the API data
-			// console.log("token", data);
-
-			// Return a second API call
-			// This one uses the token we received for authentication
-			return fetch("https://api.petfinder.com/v2/animals?type=" + animalType, {
-				headers: {
-					Authorization: data.token_type + " " + data.access_token,
-					"Content-Type": "application/x-www-form-urlencoded",
-				},
-			});
-		})
-		.then(function (resp) {
-			// Return the API response as JSON
-			return resp.json();
-		})
-		.then(function (data) {
-			// Log the pet data
-			console.log("pets", data);
-			//for loop over animals array:
-			for (let i = 0; i < data.animals.length; i++) {
-				let listElement = document.createElement("li");
-				listElement.innerHTML = data.animals[i].name;
-				animalList.appendChild(listElement);
-			}
-		})
-		.catch(function (err) {
-			// Log any errors
-			console.log("something went wrong", err);
-		});
+function onClickHandlerCat() {
+	animalType = "Cat";
+	location.href = "./index_animals.html";
+	sessionStorage.setItem("animal", animalType);
 }
+
+cat.addEventListener("click", onClickHandlerCat);
+
+function onClickHandlerBird() {
+	animalType = "Bird";
+	location.href = "./index_animals.html";
+	sessionStorage.setItem("animal", animalType);
+}
+
+bird.addEventListener("click", onClickHandlerBird);
---
+++ script_animals_located.js
@@ -0,0 +1,78 @@
+const apiKey = "BLMFFWPCzvU5LD1g0rwVrFsznX47BPiRRMhpPtTNyOSktCRiTB";
+const apiSecret = "16ouruHP8NBNMiL4R4ituHEW4GdlgwebiGTFmBah";
+const goBack = document.querySelector("#goBack");
+const animalTypeOf = sessionStorage.getItem("animal");
+let animalLocation = sessionStorage.getItem("location");
+const header1 = document.querySelector("#header1");
+const header2 = document.querySelector("#header2");
+const goHome = document.querySelector("#goHome");
+
+header1.innerHTML = `You're looking for: ${animalTypeOf}s`;
+header2.innerHTML = `located in: ${animalLocation}`;
+
+goHome.addEventListener("click", () => {
+	location.href = "./index.html";
+});
+
+goBack.addEventListener("click", () => {
+	location.href = "./index_animals.html";
+});
+
+function getAnimals(animalType, location) {
+	fetch("https://api.petfinder.com/v2/oauth2/token", {
+		method: "POST",
+		body:
+			"grant_type=client_credentials&client_id=" +
+			apiKey +
+			"&client_secret=" +
+			apiSecret,
+		headers: {
+			"Content-Type": "application/x-www-form-urlencoded",
+		},
+	})
+		.then(function (resp) {
+			// Return the response as JSON
+			return resp.json();
+		})
+		.then(function (data) {
+			// Log the API data
+			// console.log("token", data);
+
+			// Return a second API call
+			// This one uses the token we received for authentication
+			return fetch(
+				"https://api.petfinder.com/v2/animals?type=" +
+					animalType +
+					"&location=" +
+					location,
+				{
+					headers: {
+						Authorization: data.token_type + " " + data.access_token,
+						"Content-Type": "application/x-www-form-urlencoded",
+					},
+				}
+			);
+		})
+		.then(function (resp) {
+			// Return the API response as JSON
+			return resp.json();
+		})
+		.then(function (data) {
+			// Log the pet data
+			console.log("pets", data);
+			//remove old list items
+			animalList.innerHTML = "";
+			//for loop over animals array:
+			for (let i = 0; i < data.animals.length; i++) {
+				let listElement = document.createElement("li");
+				listElement.innerHTML = data.animals[i].name;
+				animalList.appendChild(listElement);
+			}
+		})
+		.catch(function (err) {
+			// Log any errors
+			console.log("something went wrong", err);
+		});
+}
+
+getAnimals(animalTypeOf, animalLocation);
